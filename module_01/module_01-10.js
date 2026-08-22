9f4123b291e3462d26f738aa89ff51e6c322a770
sajal-mazumder <sajalmazumder47@gmail.com>
2026-08-22 23:24:35 +0600

    second commit

--- module_01/module_01-10.js
+++ module_01/module_01-10.js
@@ -4,19 +4,42 @@
 
 // 1-4 string works
 
-let sajal = 'Sajal Mazumder' + '\n';
-let borsa = `Borsa Rani Roy`+ '\n';
-let we = 'Sajal and "Borsa" are spouse.'+ '\n';
-let weAre = "We are \"husband and wife\" "
-console.log(sajal, borsa, we, weAre);
-console.log(`${sajal} ${borsa} ${we}`);
+// let sajal = 'Sajal Mazumder' + '\n';
+// let borsa = `Borsa Rani Roy`+ '\n';
+// let we = 'Sajal and "Borsa" are spouse.'+ '\n';
+// let weAre = "We are \"husband and wife\" "
+// console.log(sajal, borsa, we, weAre);
+// console.log(`${sajal} ${borsa} ${we}`);
 
 
 // 1-7 function
 
-function total (price, quantity) {
+// function total (price, quantity) {
+//     let grandTotal = price * quantity;
+//     return grandTotal;
+// }
+// let bill = total(10, 20);
+// let save = 500 - bill;
+// console.log(bill, save);
+
+// function hello (name) {
+//     console.log(`Hello ${name}`)
+// }
+// hello("Sajal. How are You?");
+
+
+// 1-8 arrow function
+
+let total = (price , quantity) => {
     let grandTotal = price * quantity;
     return grandTotal;
 }
-let bill = total(10, 20);
-console.log(bill)
+let bill = total(20, 20);
+let save = 500 - bill;
+console.log(bill, save);
+
+// one line arrow function do not need return
+
+let add = (a, b) => a + b;
+let sum = add( 50, 100);
+console.log(sum);
