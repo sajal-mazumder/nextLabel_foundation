73b8a0fcc3581e9db88a46e7bfa2d138f6c998b1
sajal-mazumder <sajalmazumder47@gmail.com>
2026-08-24 22:35:38 +0600

    some update

--- module_02/object.js
+++ module_02/object.js
@@ -7,4 +7,36 @@ let user = {
     birth: '15/ 02/ 1991',
     education: 'MSc'
 }
-console.log(user.firstName);
+// console.log(user.firstName); // dot notation
+// console.log(user['birth']);  // bracket notation
+
+// object methods
+delete user.index; // deleting element
+user.education = 'MSc in Botany'; // adding values
+user.education = {
+    ssc: 'Sandwip',
+    hsc: 'Feni',
+    honors: 'Chattagram College'
+}  // updating data
+
+// console.log(user);
+// console.log(user.keys()) // error
+// console.log(Object.keys(user)); // keys result as an array
+// console.log(Object.values(user));  // values result as an array
+// console.log(Object.entries(user));  // key values result as an array.
+
+// nested array and objects
+
+let arr = ['sajal', 'ramesh',['abhro', 'shubro'],{
+    name: 'toyota',
+    model: 'b25',
+    color: 'black',
+    showroom: {
+        dhaka: 'dhanmondi',
+        kumilla: 'sadar',
+        chattagram: 'gec',
+        manager: ['rahim','karim','babul']
+    }
+}];
+arr[3].showroom.manager[0] = 'akter'  // update data
+console.log(arr[3].showroom.manager[0]);
