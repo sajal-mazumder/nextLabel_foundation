5403704ecdf594cf132e3c96216381f0b45f511e
sajal-mazumder <sajalmazumder47@gmail.com>
2026-08-24 21:26:31 +0600

    new update

--- module_01/module_01-10.js
+++ module_01/module_01-10.js
@@ -30,16 +30,42 @@
 
 // 1-8 arrow function
 
-let total = (price , quantity) => {
-    let grandTotal = price * quantity;
-    return grandTotal;
-}
-let bill = total(20, 20);
-let save = 500 - bill;
-console.log(bill, save);
+// let total = (price , quantity) => {
+//     let grandTotal = price * quantity;
+//     return grandTotal;
+// }
+// let bill = total(20, 20);
+// let save = 500 - bill;
+// console.log(bill, save);
 
 // one line arrow function do not need return
 
-let add = (a, b) => a + b;
-let sum = add( 50, 100);
-console.log(sum);
+// let add = (a, b) => a + b;
+// let sum = add( 50, 100);
+// console.log(sum);
+
+
+// let weight = process.argv[2];
+// let height = process.argv[3];
+
+// function calculateBMI (weight, height) {
+//     let bmi = weight / (height * height);
+//     return bmi;
+// }
+// console.log(calculateBMI(weight, height));
+
+
+function calculateBMI (weight, height) {
+    let bmi = weight / (height * height);
+    if (bmi < 18) {
+        console.log('Khub chikon.')
+    }else if (bmi <= 25) {
+        console.log('Good position')
+    }else if (bmi <= 30) {
+        console.log('Risk zone.')
+    }else{
+        console.log('Obesity.')
+    }
+    return bmi.toFixed(3);
+}
+console.log(calculateBMI(70, 1.5));
---
+++ module_02/array.js
@@ -0,0 +1,13 @@
+
+let name = ['Sajal', 'Borsa', 'Nayan', 'Apon'];
+
+
+// array methods
+
+// name.push('Shubro'); // adding new elements after the last index.
+// name.pop(); // delete the last index element
+// name.unshift('Shubro')   // adding new element in the zero index
+// name.shift();  // delete zero index element 
+
+name.splice(2, 0, 'Abhro')
+console.log(name);
