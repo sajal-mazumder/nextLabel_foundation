3854adf26ea0391aa05045dea85e78ddf0b34930
sajal-mazumder <sajalmazumder47@gmail.com>
2026-08-26 23:37:28 +0600

    some update

--- module_03/destructuring.js
+++ module_03/destructuring.js
@@ -24,12 +24,22 @@ let student1 = {
         hsc: 3.60,
         honors: '2nd class',
         masters: '1st class'
-    }
+    },
+    colors: ['red', 'green', 'blue']
 }
 // console.log(student);
 
 let {stName, id, age, religion, degree, degree:{ssc, hsc, honors, masters}} = student;
 console.log(degree, ssc);
 
-let {stName: newName, id: newId, age: newAge, religion:religions, degree: degrees, degree:{ssc:sscs, hsc:hscs, honors:honorss, masters:masterss}} = student1;
-console.log(newName, newId)
+// variable update : alias
+
+let {stName: newName, id: newId, age: newAge, religion:religions, degree: degrees, degree:{ssc:sscs, hsc:hscs, honors:honorss, masters:masterss}, colors:[first, second, third]} = student1;
+console.log(stName, newId, third); 
+
+// array destructuring
+
+let arr = [2, 4, 5, [55, 6, 76, 550], 34];
+
+let [first1, second1, third1,[fer, sec, thir, four], fifth] = arr;
+console.log(fer, second1, four);
