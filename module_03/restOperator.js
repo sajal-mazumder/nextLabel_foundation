844e36fd5a0d695f1fc9cf93e960a5395d4cdc30
sajal-mazumder <sajalmazumder47@gmail.com>
2026-08-27 21:15:26 +0600

    some update

---
+++ module_03/restOperator.js
@@ -0,0 +1,34 @@
+// console.log(110);
+
+// rest operator is oposite of spread operator
+// it can apply in function, array, object etc.
+
+function add(...numbers){
+    let total = 0;
+    for(num of numbers){
+        total += num;
+    }
+    return total;
+}
+// console.log(add(1,2,3))
+// console.log(`Total of numbers is ${add(1,2,3)}`);
+
+// object rest operator
+
+let obj2 = {
+    degree:{
+        ssc: 4.56,
+        hsc: 3.60,
+        honors: 2,
+        masters: 1
+    },
+    instution: {
+        school: 'Sandwip',
+        college: 'Feni',
+        varsity: 'Chattagram'
+    }
+}
+
+// let {instution,...remain} = obj2;
+let {instution: {school, ...remain}} = obj2 // by destructuring object.
+console.log(remain);
--- module_03/spreadOperator.js
+++ module_03/spreadOperator.js
@@ -6,3 +6,32 @@ let arr = [ 2, 4, 5, 65, 654, 43, 34];
 
 let arr2 = [...arr, 12, 32, 33];
 console.log(arr2);
+
+let obj1 = {
+    name: 'Sajal',
+    id: 34,
+    age: 36,
+    religion: 'Hindhu',
+    maritalStatus: 'Married'
+}
+
+let obj2 = {
+    degree:{
+        ssc: 4.56,
+        hsc: 3.60,
+        honors: 2,
+        masters: 1
+    },
+    instution: {
+        school: 'Sandwip',
+        college: 'Feni',
+        varsity: 'Chattagram'
+    }
+}
+
+let obj3 = {
+    ...obj1,
+    ...obj2,
+    favColor: 'Green'
+}
+console.log(obj3);
