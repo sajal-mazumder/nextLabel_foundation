5e0dff80ac25aca1f37e01e4bc55046bf321bfc5
sajal-mazumder <sajalmazumder47@gmail.com>
2026-08-27 20:49:40 +0600

    some update

---
+++ module_03/spreadOperator.js
@@ -0,0 +1,8 @@
+// console.log(99);
+
+// ... three dots spread old array or object to a new array or object
+
+let arr = [ 2, 4, 5, 65, 654, 43, 34];
+
+let arr2 = [...arr, 12, 32, 33];
+console.log(arr2);
